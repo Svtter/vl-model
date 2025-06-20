c4a916112b6f2d337aa18aadc397a84ec817db91
svtter <svtter@qq.com>
2025-06-20 12:26:27 +0800

    feat: using continoues model-group.

--- src/mr/vlask.py
+++ src/mr/vlask.py
@@ -1,6 +1,7 @@
 import base64
 
 from mr import LocationResposne
+from mr.group import ModelGroup, siliconflow_group
 from mr.prompt import MeterBoxPrompt
 from mr.tools import JSONFormatter
 from vl_model.client import get_client
@@ -9,16 +10,16 @@ from vl_model.client import get_client
 class VlAsk(object):
   """用于使用不支持 tool 的模型，进行多轮对话。"""
 
-  def __init__(self, model_name: str):
+  def __init__(self, model_group: ModelGroup):
     self.client = get_client()
-    self.model_name = model_name
+    self.model_group = model_group
 
   def ask(self, image_path: str) -> LocationResposne:
     with open(image_path, "rb") as image_file:
       image_data = image_file.read()
 
     response = self.client.chat.completions.create(
-      model=self.model_name,
+      model=self.model_group.vision or "",
       messages=[
         {"role": "system", "content": MeterBoxPrompt().get_prompt()},
         {
@@ -33,6 +34,8 @@ class VlAsk(object):
       ],
     )
     content = response.choices[0].message.content or ""
+    json_formatter = JSONFormatter(group=self.model_group)
+    content = json_formatter.format_response(content)
     if content:
       return LocationResposne.model_validate_json(content)
     else:
@@ -40,5 +43,5 @@ class VlAsk(object):
 
 
 if __name__ == "__main__":
-  vlask = VlAsk("Qwen/Qwen2.5-VL-72B-Instruct")
+  vlask = VlAsk(siliconflow_group)
   vlask.ask("./resources/meter-2.jpg")
