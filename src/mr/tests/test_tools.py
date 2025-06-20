7e7d2d3c0ec7f8c5edae648e7e4dcfc3c0d71fee
svtter <svtter@qq.com>
2025-06-20 12:24:12 +0800

    feat: pass the json test.

--- src/mr/tests/test_tools.py
+++ src/mr/tests/test_tools.py
@@ -1,3 +1,6 @@
+import json
+
+from mr.group import siliconflow_group
 from mr.tools import JSONFormatter
 
 
@@ -6,5 +9,12 @@ def test_json_formatter():
   This is a dirty json:
   {'test': 'test'}
   """
-  json_formatter = JSONFormatter()
-  assert json_formatter.format_response(dirty_json) == """{"test": "test"}"""
+
+  json_formatter = JSONFormatter(group=siliconflow_group)
+
+  result = json_formatter.format_response(dirty_json)
+
+  try:
+    assert json.loads(result) == {"test": "test"}
+  except json.JSONDecodeError:
+    assert False, f"json.JSONDecodeError: {result}"
